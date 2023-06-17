18a61cdd88410dcb218a3b11de72cce6ec61a2ce
PoopieJoe <henry.slegers@hotmail.com>
2023-06-17 11:40:07 +0200

    added links

--- Engine/Engine/EventDispatcher.h
+++ Engine/Engine/EventDispatcher.h
@@ -7,6 +7,9 @@
 * Then the event must be registered with the [EventDispatcher]
 * When an Event occurs, all listeners with a handler for that type
 * of event have said handler invoked by the dispatcher
+* https://codereview.stackexchange.com/questions/200049/simple-event-dispatcher
+* https://stackoverflow.com/questions/14033271/design-event-dispatcher-event-source-event-watcher-with-type-safety-in-c
+* 
 */
 
 enum class FluxworksEventType : uint64_t
